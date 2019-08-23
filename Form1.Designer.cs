6a9fa28efa0d910051e5f47f19b64779b4b76e99
pjdsant@gmail.com <pjdsant@gmail.com>
2019-08-22 23:08:46 -0300

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace FormTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtIniRange = new System.Windows.Forms.TextBox();
+            this.txtEndRange = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(70, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(259, 99);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 476);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(447, 80);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(290, 99);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // txtIniRange
+            // 
+            this.txtIniRange.Location = new System.Drawing.Point(70, 208);
+            this.txtIniRange.Name = "txtIniRange";
+            this.txtIniRange.Size = new System.Drawing.Size(212, 31);
+            this.txtIniRange.TabIndex = 3;
+            // 
+            // txtEndRange
+            // 
+            this.txtEndRange.Location = new System.Drawing.Point(70, 257);
+            this.txtEndRange.Name = "txtEndRange";
+            this.txtEndRange.Size = new System.Drawing.Size(212, 31);
+            this.txtEndRange.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(992, 766);
+            this.Controls.Add(this.txtEndRange);
+            this.Controls.Add(this.txtIniRange);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtIniRange;
+        private System.Windows.Forms.TextBox txtEndRange;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using LikeWater.WinHCtl.WinApi;
+using System;
+using System.Windows.Forms;
+
+namespace FormTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            WinApiX winApiX = new WinApiX();
+
+            label1.Text = winApiX.GetListItem("System Interact", 0, 0);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            WinApiX winApiX = new WinApiX();
+
+            label1.Text = winApiX.GetListItem("System Interact", 0, 0);
+
+        }
+    }
+}
